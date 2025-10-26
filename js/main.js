94bcae1f36b6e186e1eba068efb5894ae4234d65
πr <98342698+lmsflms@users.noreply.github.com>
2025-10-26 11:26:47 +0100

    updated code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-const greenland = document.getElementById("Greenland");
-const blueland = document.getElementById("Blueland");
+const keirstand = document.getElementById("Keirstand");
+const svalkuschtania = document.getElementById("Svalkuschtania");
 const territoryText = document.getElementById("territoryText");
 
-outlinePaths(greenland);
-outlinePaths(blueland);
+outlinePaths(keirstand);
+outlinePaths(svalkuschtania);
 
 function outlinePaths(group) {
 	const paths = group.querySelectorAll("path");
 	paths.forEach(path => {
 		path.onmouseover = () => {group.classList.add("highlight"); territoryText.style.visibility = "visible"; territoryText.textContent=group.id};
 		path.onmouseout  = () => {group.classList.remove("highlight"); territoryText.style.visibility = "hidden"};
-		//path.onclick = () => {group.classList.add("select")};
 	});
 }
