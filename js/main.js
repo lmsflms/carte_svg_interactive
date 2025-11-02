602043efa3519a2a24e7113d6dd534c294d80ff6
πr <98342698+lmsflms@users.noreply.github.com>
2025-11-02 10:39:20 +0100

    updated code

--- js/main.js
+++ js/main.js
@@ -9,32 +9,34 @@ function outlinePaths(group) {
     paths.forEach(path => {
         path.onmouseover = () => {
             group.classList.add("highlight");
-            territoryText.style.visibility = "visible";
-            territoryText.textContent = group.id;
         };
 
         path.onmouseout = () => {
             group.classList.remove("highlight");
-            territoryText.style.visibility = "hidden";
         };
 
         path.onclick = (e) => {
             e.preventDefault();
             if (selectedGroup && selectedGroup !== group) {
                 selectedGroup.classList.remove("selected");
+                territoryText.style.visibility = "hidden";
             }
             group.classList.add("selected");
+            territoryText.style.visibility = "visible";
+            territoryText.textContent = group.id;
+
             selectedGroup = group;
 
             group.parentNode.appendChild(group);
         };
     });
 }
-
+ 
 document.addEventListener("contextmenu", (e) => {
     e.preventDefault();
     if (selectedGroup) {
         selectedGroup.classList.remove("selected");
+        territoryText.style.visibility = "hidden";
         selectedGroup = null;
     }
 });
