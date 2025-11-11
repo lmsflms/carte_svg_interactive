a6c7de92aee43dcfeea8cd61b89ac5104ee76cf7
πr <98342698+lmsflms@users.noreply.github.com>
2025-11-11 14:22:25 +0100

    updated code

--- js/main.js
+++ js/main.js
@@ -1,62 +1,47 @@
 fetch("map/map.svg")
-    .then(r => r.text())
-    .then(svgData => {
-        document.getElementById("mapDiv").insertAdjacentHTML("beforeend", svgData);
-        loadMap();
-    })
+  .then(r => r.text())
+  .then(svgData => {
+	document.getElementById("mapView").insertAdjacentHTML("beforeend", svgData);
+	loadMap();
+  });
 
-function loadMap(){
-    const groups = document.querySelectorAll(".territory");
-    let selectedGroup = null;
+function loadMap() {
+  const groups = document.querySelectorAll(".territory");
+  const territoryLabel = document.getElementById("territory-label");
+  let selectedGroup = null;
 
-    groups.forEach(group => outlinePaths(group));
+  groups.forEach(group => outlinePaths(group));
 
-    function outlinePaths(group) {
-        const paths = group.querySelectorAll("path");
+  function outlinePaths(group) {
+	const paths = group.querySelectorAll("path");
 
-        paths.forEach(path => {
-            path.onmouseover = () => {
-                group.classList.add("highlight");
-            };
+	paths.forEach(path => {
+	  path.onmouseover = () => group.classList.add("highlight");
+	  path.onmouseout = () => group.classList.remove("highlight");
 
-            path.onmouseout = () => {
-                group.classList.remove("highlight");
-            };
+	  path.onclick = e => {
+		e.preventDefault();
+		if (selectedGroup && selectedGroup !== group) {
+		  selectedGroup.classList.remove("selected");
+		  territoryLabel.style.visibility = "hidden";
+		}
 
-            path.onclick = (e) => {
-                e.preventDefault();
-                if (selectedGroup && selectedGroup !== group) {
-                    selectedGroup.classList.remove("selected");
-                    territoryText.style.visibility = "hidden";
-                }
-                group.classList.add("selected");
-                territoryText.style.visibility = "visible";
-                territoryText.textContent = group.id;
+		group.classList.add("selected");
+		territoryLabel.style.visibility = "visible";
+		territoryLabel.textContent = group.id;
 
-                selectedGroup = group;
+		selectedGroup = group;
+		group.parentNode.appendChild(group);
+	  };
+	});
+  }
 
-                group.parentNode.appendChild(group);
-            };
-        });
-    }
-    
-    document.addEventListener("contextmenu", (e) => {
-        e.preventDefault();
-        if (selectedGroup) {
-            selectedGroup.classList.remove("selected");
-            territoryText.style.visibility = "hidden";
-            selectedGroup = null;
-        }
-    });
-}
-
-let map = 0;
-var numMap = 1;
-
-function changeMap(){
-    map++;
-    map = map % numMap;
-
-    const img = document.querySelector('.pngMap');
-    img.src = `map/${map}.png`;
+  document.addEventListener("contextmenu", e => {
+	e.preventDefault();
+	if (selectedGroup) {
+	  selectedGroup.classList.remove("selected");
+	  territoryLabel.style.visibility = "hidden";
+	  selectedGroup = null;
+	}
+  });
 }
